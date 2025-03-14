116619bac44ab2f1972081828e402951e74acab5
Monica Kiyota <monica.kiyota@gmail.com>
2025-03-14 11:11:42 -0300

    :sparkles: Adição de um switch-case para as operações

--- calculadora/bin/calculadora.dart
+++ calculadora/bin/calculadora.dart
@@ -1,12 +1,10 @@
 import 'dart:io';
 
 void main() {
-  print("Digite o primeiro número:");
-  double numeroUm = double.parse(stdin.readLineSync()!);
-
-  print("Digite o segundo número:");
-  double numeroDois = double.parse(stdin.readLineSync()!);
-
+  String operacao = "";
+  double numeroUm = 0;
+  double numeroDois = 0;
+  
   void soma() {
     print(numeroUm + numeroDois);
   } 
@@ -23,8 +21,44 @@ void main() {
     print(numeroUm / numeroDois);
   }
 
-  soma();
-  subtracao();
-  multiplicacao();
-  divisao();
+  void calcular(){
+    switch(operacao){
+    case "+":
+    soma();
+
+    case "-":
+    subtracao();
+
+    case "*":
+    multiplicacao();
+
+    case "/":
+    divisao();
+    break;
+    }
+  }
+
+  print("Digite o primeiro valor");
+  String? entrada = stdin.readLineSync();
+  if (entrada != null){
+    if(entrada != ""){
+      numeroUm = double.parse(entrada);
+    }
+  }
+
+  print("Digite o segundo valor");
+  entrada = stdin.readLineSync();
+  if (entrada != null){
+    if(entrada != ""){
+      numeroDois = double.parse(entrada);
+    }
+  }
+
+  print("Digite uma operação");
+  entrada = stdin.readLineSync();
+  if(entrada != null){
+    operacao = entrada;
+  }
+
+  calcular();
 }
